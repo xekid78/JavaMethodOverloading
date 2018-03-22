80c65f7e600ab50157bb0e4d740c432c47c2bd79
Makoto Tanabe <xekid78@outlook.com>
2018-03-22 17:31:30 +0900

    Add files via upload

---
+++ Overload.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+public class Overload {
+
+	public static void main(String[] args) {
+		attack();
+		attack("スライム");
+		attack(10);
+		attack("ドラゴン", 3);
+
+	}
+
+	public static void attack() {
+		System.out.println("勇者は、敵を攻撃した。");
+
+	}
+
+	public static void attack(String target) {
+		System.out.println("勇者は、" + target + "を攻撃した。");
+
+	}
+
+	public static void attack(int number) {
+		System.out.println("勇者は、" + number + "匹の敵を攻撃した。");
+
+	}
+
+	public static void attack(String target, int number) {
+		System.out.println("勇者は、" + number + "匹の" + target + "を攻撃した。");
+
+	}
+
+}
